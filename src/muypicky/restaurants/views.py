f94a09fef247ca4e9838f3d2821b5e4f5d3cd169
Rommel Rico <me@rommelrico.com>
2017-11-06 09:20:30 -0800

    Using the Template View

--- src/muypicky/restaurants/views.py
+++ src/muypicky/restaurants/views.py
@@ -1,40 +1,21 @@
 import random
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
 
-# Create your views here.
-# Function based view
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 100000000),
-        random.randint(0, 100000000),
-        random.randint(0, 100000000)
-    ]
-    condition_bool_item = False
-    if condition_bool_item:
-        num = random.randint(0, 100000000)
-    context = {
-        "num": num,
-        "some_list": some_list
-    }
-    return render(request, "home.html", context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        num = random.randint(0, 1000)
+        some_list = [
+            random.randint(0, 1000),
+            random.randint(0, 1000),
+            random.randint(0, 1000)
+        ]
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+        return context
 
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context)
-
-
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context)
-
-
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, "contact.html", context)
