91bbece3cdcf8d0097d67f2106548a8a5f0e5c82
Rommel Rico <rommeltj@gmail.com>
2017-11-05 20:42:15 -0800

    Class-based views

--- src/muypicky/restaurants/views.py
+++ src/muypicky/restaurants/views.py
@@ -1,5 +1,6 @@
 import random
 from django.shortcuts import render
+from django.views import View
 
 
 # Create your views here.
@@ -31,3 +32,9 @@ def contact(request):
     context = {
     }
     return render(request, "contact.html", context)
+
+
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, "contact.html", context)
