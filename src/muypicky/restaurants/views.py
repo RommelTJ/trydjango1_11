0968055577ca0e412ac352409aeb60897be89b9d
Rommel Rico <rommeltj@gmail.com>
2017-11-05 19:58:14 -0800

    Render a Django Template

--- src/muypicky/restaurants/views.py
+++ src/muypicky/restaurants/views.py
@@ -1,23 +1,9 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 # Function based view
 def home(request):
-    html_var = 'f strings'
-    html = f"""
-    <!DOCTYPE html>
-
-    <head>
-
-    </head>
-
-    <body>
-    <h1>Hello World</h1>
-    <p>This is {html_var} coming through.</p>
-    </body>
-
-    </html>
-    """
-    # return render(request, "home.html", {})
-    return HttpResponse(html)
+    num = random.randint(0, 100)
+    return render(request, "base.html", {"html_var": "context variable", "num": num})
