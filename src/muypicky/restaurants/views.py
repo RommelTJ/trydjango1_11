ea2edd1d10a559595a738f322633584ed7542857
Rommel Rico <me@rommelrico.com>
2017-11-08 08:48:37 -0800

    Generic List View

--- src/muypicky/restaurants/views.py
+++ src/muypicky/restaurants/views.py
@@ -1,11 +1,27 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import RestaurantLocation
 
 
 def restaurant_listview(request):
-    template_name = 'restaurants/restaurants_list.html'
+    template_name = 'restaurants/restaurantlocation_list.html'
     queryset = RestaurantLocation.objects.all()
     context = {
         "object_list": queryset
     }
     return render(request, template_name, context)
+
+
+class RestaurantListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__iexact=slug) |
+                Q(category__icontains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
