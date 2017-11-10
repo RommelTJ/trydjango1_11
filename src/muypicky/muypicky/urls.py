72f7b6671b963fd9970d7fa256c03fd140ee8158
Rommel Rico <rommeltj@gmail.com>
2017-11-09 21:22:00 -0800

    Associate User to Form Data in FBV

--- src/muypicky/muypicky/urls.py
+++ src/muypicky/muypicky/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 from restaurants.views import (
+    restaurant_createview,
     restaurant_listview,
     RestaurantListView,
     RestaurantDetailView,
@@ -13,7 +14,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^restaurants/$', RestaurantListView.as_view()),
-    url(r'^restaurants/create/$', RestaurantCreateView.as_view()),
+    url(r'^restaurants/create/$',   restaurant_createview ),
     url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
