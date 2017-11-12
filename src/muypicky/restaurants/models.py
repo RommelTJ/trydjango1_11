46ec214120d7af30c14029b170810a6e61364166
Rommel Rico <rommeltj@gmail.com>
2017-11-11 19:33:48 -0800

    Adding a Robust Search

--- src/muypicky/restaurants/models.py
+++ src/muypicky/restaurants/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.core.urlresolvers import reverse
 
@@ -10,6 +11,30 @@ User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
+class RestaurantLocationQuerySet(models.query.QuerySet):
+    def search(self, query): #RestaurantLocation.objects.all().search(query) #RestaurantLocation.objects.filter(something).search()
+        if query:
+            query = query.strip()
+            return self.filter(
+                Q(name__icontains=query)|
+                Q(location__icontains=query)|
+                Q(location__iexact=query)|
+                Q(category__icontains=query)|
+                Q(category__iexact=query)|
+                Q(item__name__icontains=query)|
+                Q(item__contents__icontains=query)
+            ).distinct()
+        return self
+
+
+class RestaurantLocationManager(models.Manager):
+    def get_queryset(self):
+        return RestaurantLocationQuerySet(self.model, using=self._db)
+
+    def search(self, query): #RestaurantLocation.objects.search()
+        return self.get_queryset().search(query)
+
+
 class RestaurantLocation(models.Model):
     owner = models.ForeignKey(User) # class_instance.model_set.all()
     name = models.CharField(max_length=120)
@@ -20,6 +45,8 @@ class RestaurantLocation(models.Model):
     slug = models.SlugField(null=True, blank=True)
     #my_date_field = models.DateField(auto_now=False, auto_now_add=False)
 
+    objects = RestaurantLocationManager()
+    
     def __str__(self):
         return self.name
 
