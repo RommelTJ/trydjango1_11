79932b26bba5d57c1de29c3718a1d1ca7bbc1d78
Rommel Rico <rommeltj@gmail.com>
2017-11-12 11:39:51 -0800

    Register View

--- src/muypicky/profiles/models.py
+++ src/muypicky/profiles/models.py
@@ -32,6 +32,9 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+    def send_activation_email(self):
+        print("Activation")
+        pass
 
 def post_save_user_receiver(sender, instance, created, *args, **kwargs):
     if created:
